a518c1228e32184e669bd09aef8ac23850fff1ae
elizahyde <elizabeth@elizahyde.com>
2014-03-18 09:32:04 -0700

    is number and is operator passing

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -3,17 +3,32 @@ require './stack.rb'
 # Do not use a ruby array!  Add your list class instead
 
 class RPNCalculator
- 
-  # Parse should return a list class that you defined, not a ruby array 
+
+  # Parse should return a list class that you defined, not a ruby array
+  # use split to return array
+
   def self.parse(rpn_string)
+    new_list = rpn_string.split(" ")
+    return new_list
   end
 
   def self.evaluate(rpn_list)
+    # fill in code
   end
 
   def self.is_operation(operation)
+    if ["-", "/", "*", "+", "^"].include?(operation)
+      return true
+    else
+      return false
+    end
   end
-  
+
   def self.is_number(num)
+    if num.match(/[-]?\d+/)
+      return true
+    else
+      return false
+    end
   end
 end
--- stack.rb
+++ stack.rb
@@ -1,5 +1,59 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
+class Node
+    attr_accessor :value, :next, :previous
+  def initialize(value)
+    @value = value
+    @next = nil
+    @previous = nil
+  end
+end
 
 class Stack
+  attr_accessor :length
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+  def push(value)
+   if @head == nil
+      @head = Node.new(value)
+      @tail = @head
+   else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+   end
+      @length += 1
+  end
+
+  # Returns the value that is popped off
+  # or nil if none exists
+  def pop
+    if @length == 0
+      return nil
+    elsif @length == 1
+      value = @head.value
+      @head = nil
+      @tail = nil
+      @length = 0
+      return value
+    else
+     value = @tail.value
+     new_tail = @tail.previous
+     @tail = nil
+     @tail = new_tail
+     @tail.next = nil
+     @length -= 1
+     return value
+    end
+
+  end
+
+
 end
+
